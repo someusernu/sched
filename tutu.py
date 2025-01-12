f5fdc7cf7e78943ad8eff669f6feea084cb12df9
someusernu <105249610+someusernu@users.noreply.github.com>
2025-01-12 03:28:36 +0300

    added trip class and fill it with values

--- tutu.py
+++ tutu.py
@@ -14,8 +14,47 @@ class Event:
         self.changes = changes
 
 
+class LinkedEvent():
+    def __init__(self):
+        self.text = ''
+        self.link = ''
+
+
+class Station():
+    def __init__(self):
+        self.name = ''
+        self.id = -1
+
+
 def tutu_today():
     return datetime.datetime.now().strftime('%d.%m.%Y')
+
+
+class Trip():
+    def __init__(self, first, second, third, fourth, fifth, sixth, seventh, eighth, nineth):
+        self.departure = self.get_departure(first)
+        self.arrival = self.get_departure(second)
+        self.scheduled = third.string
+        self.time = fourth.string
+        self.route = self.get_route(fifth)
+        self.price = sixth.string
+        self.actual_mov = seventh.string
+        self.actual_dep = self.get_departure(eighth)
+        self.actual_arr = self.get_departure(nineth)
+
+    def get_departure(self, cell):
+        departure = LinkedEvent()
+        departure.text = cell.a.string
+        departure.link = cell.a['href']
+        return departure
+
+    def get_route(self, cell):
+        route = list()
+        for point in cell.find_all('a'):
+            station = Station()
+            station.name = point.string
+            station.id = point['href'][point['href'].find('=') + 1 :]
+            route.append(station)
 
 
 async def get_url_today(departure=43706, arrival=41406):
@@ -32,16 +71,19 @@ def get_roster(txt):
         unusual = True
     else:
         unusual = False
-    tgs = srch.find_all(class_='l-etrain__main_timetable')
-    tag = tgs[0]
-    srch = tag.find_all_next('tbody', recursive=False)
-    items = srch[0].find_all_next('tr', recursive=False)
-    for item in items:
-        print(item)
-        cells = item.find_all_next('a', recursive=False)
-        for cell in cells:
-            print(cell)
-        print('_________')
+    tgs = srch.find(class_='l-etrain__main_timetable').tbody
+    print(tgs.prettify())
+    items = tgs.find_all_next('tr', recursive=False)
+    full_sched = list()
+    for i in range(len(items) - 1):
+        cells = items[i].find_all_next('td', recursive=False, limit=9)
+        if len(cells) == 7:
+            trip = Trip(*cells, cells[0], cells[1])
+        else:
+            trip = Trip(*cells)
+        full_sched.append(trip)
+    z = 0
+
 
 
 async def main(test=True):
@@ -53,4 +95,4 @@ async def main(test=True):
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(main(False))
