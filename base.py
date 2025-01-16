326593bb25079d59bcc84324f35a57b6e7928b0c
someusernu <foh@pan-therra.ru>
2025-01-17 00:45:50 +1200

    coded db creation just instance without tables

--- base.py
+++ base.py
@@ -11,11 +11,29 @@ async def base_creds():
     return base_creds
 
 
-async def base_connect():
+async def base_connect(creation=False):
     base_cred = await base_creds()
-    conn = await psycopg.AsyncConnection.connect(user=base_cred['user'], password=base_cred['password'])
+    if creation:
+        dbname = 'postgres'
+    else:
+        dbname = base_cred['base']
+    conn = await psycopg.AsyncConnection.connect(user=base_cred['user'], password=base_cred['password'],
+                                                 dbname=dbname, host=base_cred['host'], port=base_cred['port'])
     cursor = conn.cursor()
     return conn, cursor
+
+
+async def create_base():
+    base_cred = await base_creds()
+    conn, cur = await base_connect(True)
+    await cur.execute('create database %s;' % base_cred['base'])
+    await conn.commit()
+    await cur.execute('create user schedler;')
+    await conn.commit()
+    await cur.execute('grant all privileges on %s to schedler;' % base_cred['base'])
+    await conn.commit()
+    await cur.close()
+    await conn.close()
 
 
 def newbase():
